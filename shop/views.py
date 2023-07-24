8fbef25a4d8844c930fe532bd8736acef6319880
Vlad Smek <smek1negithub@gmail.com>
2023-07-24 12:10:18 +0300

    feat: cart for products
    add/remove product to cart, change product quantity

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from cart.forms import CartAddProductForm
 
 from .models import Category, Product
 
@@ -16,11 +17,16 @@ def product_list(request, category_slug=None):
     return render(
         request,
         "shop/product/list.html",
-        {"category": category, "categories": categories, "products": products}
+        {"category": category, "categories": categories, "products": products},
     )
 
 
 def product_detail(request, id, slug):
     """One product"""
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, "shop/product/detail.html", {"product": product})
+    cart_product_form = CartAddProductForm()
+    return render(
+        request,
+        "shop/product/detail.html",
+        {"product": product, "cart_product_form": cart_product_form},
+    )
